b2ec7e9c60dc7294b9f1f825d286f710f9ff7048
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-01-28 21:19:22 +0530

    with initial commands json file came

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-const heading=React.createElement("h1",{},"Hello World! from react ");
+
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id:"child"},React.createElement("h1",{},"I am a h1 tag")))
+
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
