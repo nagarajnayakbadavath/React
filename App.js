548652e8aa1d45b718a6a5f786cda47041770b58
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-01-28 22:27:51 +0530

    About Parcel operations and documentations

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},React.createElement("h1",{},"I am a h1 tag")))
+    React.createElement("div",{id:"child"},React.createElement("h1",{},"I Love you Dad")))
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-This is React project
+This is React project.
+<br>
+
+#parcel 
+-Dev Build
+-Local Server
+-HMR- Hot Module Replacement
+-File Watching Algorithm 
+-Faster Builds
+-Error Handling
+
+-visit and read about the documentation of parcel
