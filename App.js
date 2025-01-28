fd965a69aa84cd44875ae2c8c4fbc603da819116
Badavath Nagaraju <nagarajnayakbadavath@gmail.com>
2025-01-29 00:13:15 +0530

    jsx,functional components,react element and react components

--- App.js
+++ App.js
@@ -1,9 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// const JsxHeading=()=>(
+//     <div>
+//         <h1 id="heading">Hello Man!</h1>
+//     </div>
+// );
 
-const parent=React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},React.createElement("h1",{},"I Love you Dad")))
+//Make a react title
+
+const title=(
+    <div>
+        <h1>Hey This is React Element</h1>
+    </div>
+);
+
+const Head=()=>(
+    <div>
+        <h1>Hello Man I am making the functional react to access in three different ways</h1>
+    </div>
+);
+
+//create a react function
+const Heading=()=>(
+    <div>
+        {Head()}
+        <Head/>
+        <Head></Head>
+        <h1>Hey I am Created React Function</h1>
+    </div>
+);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Heading/>);
